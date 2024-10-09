4687eda763a63332a49f205f9e854e11a5d598d7
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-10-09 23:42:05 +0530

    Update index.js

--- src/components/LandingPage/MainComponent/index.js
+++ src/components/LandingPage/MainComponent/index.js
@@ -47,8 +47,8 @@ function MainComponent() {
           </a>
           <RWebShare
             data={{
-              text: "CryptoDashboard made by Avi Vashishta using React JS.",
-              url: "https://crypto-dashboard-jan.netlify.app",
+              text: "Crypto Coin Trek made by Amarjeet Kumar using React JS.",
+              url: "https://crypto-price-trek.vercel.app",
               title: "CryptoTracker.",
             }}
             onClick={() => toast.info("App Shared!")}
